0542251551e6ff15b4605a12e0b583bdd5e0d4be
Shrey-ansh10 <shreyanshsharma1010@gmail.com>
2022-05-20 15:15:24 +0530

    JavaScript Coding Practice

---
+++ Coding Challenges/script.js
@@ -0,0 +1,18 @@
+// Coding challenge 1
+
+const massMark = 95;
+const massJohn = 85;
+
+const heightMark = 1.88;
+const heightJohn = 1.76;
+
+const bmiMark = massMark / heightMark **2;
+const bmiJhon = massJohn / (heightJohn * heightJohn);
+
+const markHigherBMI = bmiMark > bmiJhon;
+
+console.log(bmiMark, bmiJhon);
+console.log(markHigherBMI);
+
+// Coding Challenge 2
+
