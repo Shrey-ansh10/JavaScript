9fe02c55967b646d04e7d607016fccd7f8e24aef
Shreyansh Sharma <shreyanshsharma1010@gmail.com>
2024-07-24 05:40:28 +0000

    Js Basics

---
+++ JS Basics/2-Arrays.js
@@ -0,0 +1,48 @@
+// complex primitive
+// Arrays in JS
+
+/*
+// 1. Write a program prints all the even numbers in an array
+
+let num = [2, 4, 5, 7, 13, 12, 20, 26, 55, 63, 79]  // syntax to define an arrey
+
+for (i=0; i < num.length; i++)
+    {
+        if(num[i] % 2 == 0)
+            {
+                console.log('Even Number ' + num[i])
+            }
+    }
+
+// 1B. Write a program prints all the Odd numbers in an array
+
+for (i=0; i < num.length; i++)
+    {
+        if(num[i] % 2 !== 0)  // !(not) operator is used
+            {
+                console.log('Odd Number are :' + num[i])
+            }
+    }
+
+*/
+/*
+// 2. Write a program to print the biggest number in an array
+
+let numbers = [3, 5, 1, 7, 33, 20, 99, 10, 4, 55 ];
+
+let max = numbers[0];
+
+for( let i=1 ; i < numbers.length; i++)
+    {
+        if(numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+    }
+
+console.log(max);
+*/
+
+
+//------------------------------------------------------------------------------------
+
