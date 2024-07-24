355d44510cee59cc1a9b822ad3d399ede0ab920c
Shreyansh Sharma <shreyanshsharma1010@gmail.com>
2024-07-24 05:37:15 +0000

    JS basics revision-1

---
+++ Class-1/1-Basics.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+
+let age = 21;
+
+let firstName = 'Shrey';
+
+console.log(firstName + " " + age);  // string concatination
+
+
+// Some basic question problems
+
+// Q: Print number upto 25
+for ( i=0 ; i<26; i++)
+    {
+        console.log(i)
+    }
+
+
+// Q: Sum of n natural numbers
+let n = 0;
+
+for (i=0; i<=10; i++)
+    {
+        n = n + i;
+    }
+console.log(n)
